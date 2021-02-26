475620694c0ec87c9c83717ab551168f0eff9c71
Y2CS2021 <79495133+Y2CS2021@users.noreply.github.com>
2021-02-26 11:53:38 +0800

    Add files via upload

--- paint-app/a.js
+++ paint-app/a.js
@@ -1,3 +1,10 @@
 var canvasWidth = screen.width;
 var canvasHeight = screen.height;
 var happy = "DIE";
+var optionsMenuShown = false;
+
+function showOptionsMenu(value) {
+    var optionsMenu = document.getElementById('options');
+    value ? optionsMenu.style.display = 'none' : value == null ? optionsMenu.style.display = optionsMenuShown ? 'none' : 'block' : optionsMenu.style.display = 'block';
+    optionsMenuShown = !optionsMenuShown;
+}
