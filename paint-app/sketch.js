555ab126b0309b49dbecc36ff53410f00cfc90e4
Y2CS2021 <79495133+Y2CS2021@users.noreply.github.com>
2021-02-26 20:16:00 +0800

    Update sketch.js

--- paint-app/sketch.js
+++ paint-app/sketch.js
@@ -1,5 +1,5 @@
 
-var thickness = 10, colour = 'black', colours = ['red', 'green', 'blue'], index = 0, randomColour = false, optionsMenu = false, happy;
+var colour = 'black', colours = ['red', 'green', 'blue'], index = 0, randomColour = false, optionsMenu = false, happy;
 
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
