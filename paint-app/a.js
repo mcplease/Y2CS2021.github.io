f4a190023cf4467082fcbf6caec36876383e0f86
Y2CS2021 <79495133+Y2CS2021@users.noreply.github.com>
2021-02-26 12:15:42 +0800

    Add files via upload

--- paint-app/a.js
+++ paint-app/a.js
@@ -5,6 +5,8 @@ var optionsMenuShown = false;
 
 function showOptionsMenu(value) {
     var optionsMenu = document.getElementById('options');
+    var showOptionsMenuButton = document.getElementById('showOptionsMenuButton');
     value ? optionsMenu.style.display = 'none' : value == null ? optionsMenu.style.display = optionsMenuShown ? 'none' : 'block' : optionsMenu.style.display = 'block';
+    showOptionsMenuButton.style.display = 'none';
     optionsMenuShown = !optionsMenuShown;
 }
